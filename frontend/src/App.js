51d5bd8cfe04991325338be4a63035e33ee5c212
JYOTHI <131485519+JYOTHIKONDUPALLY@users.noreply.github.com>
2024-04-16 20:28:12 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import Thanks from "./components/Thanks";
 import ipConfig from "./ipConfig.json";
 
 export const config = {
-  endpoint: `https://ecommerce-backend-6.onrender.com//v1`,
+  endpoint: `https://ecommerce-backend-6.onrender.com/v1`,
 };
 
 export default function App(props) {
